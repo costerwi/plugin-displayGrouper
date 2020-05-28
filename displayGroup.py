116624a8cd576827238643baac9b34253b9904b2
Carl Osterwisch <costerwi@gmail.com>
2020-05-28 16:11:30 -0400

    Don't consider elements that are already displayed

--- displayGroup.py
+++ displayGroup.py
@@ -13,7 +13,7 @@ def addAdjacent():
     odb = session.odbs[vp.odbDisplay.name]
 
     activeNodes = vp.getActiveNodeLabels(printResults=False)
-    nActiveElements = len(vp.getActiveElementLabels(printResults=False))
+    activeElements = vp.getActiveElementLabels(printResults=False)
     nElements = 0 # Count number of elements added
 
     # TODO: Check for odb.rootAssembly elements
@@ -22,14 +22,17 @@ def addAdjacent():
     for instName, nodeLabels in activeNodes.items():
         inst = odb.rootAssembly.instances[instName]
         nodeSet = set(nodeLabels)
+        elemSet = set(activeElements.get(instName, [])) # active intance elements
         elements = []
-        N = len(inst.elements)
-        statusUpdate = 0
+        N = max(1, len(inst.elements)/100)
+        progressUpdate = 0
         for n, element in enumerate(inst.elements):
-            if time() > statusUpdate:
-                statusUpdate = time() + 2.5 # update every 2.5 seconds
+            if 0 == n%200 and time() > progressUpdate:
+                progressUpdate = time() + 2.5 # update every 2.5 seconds
                 milestone(message='Checking {}'.format(instName),
-                        percent=100*n/N)
+                        percent=n/N)
+            if element.label in elemSet:
+                continue # already displayed
             for node in element.connectivity:
                 if node in nodeSet:
                     elements.append(element.label)
@@ -42,7 +45,7 @@ def addAdjacent():
                 (instName, nodeLabels), )))
         nElements += len(elements)
 
-    print('Added {} adjacent elements.'.format(nElements - nActiveElements))
+    print('Added {} adjacent elements.'.format(nElements))
 
 if "__main__" == __name__:
     addAdjacent()
