3db71e1e750d192577fca6c41b37486d403cba90
Carl Osterwisch <costerwi@gmail.com>
2024-04-13 08:23:21 -0400

    Add several new methods

--- displayGroup.py
+++ displayGroup.py
@@ -8,6 +8,9 @@ from time import time
 from abaqus import session, milestone, getInput, getWarningReply, CANCEL
 import displayGroupOdbToolset as dgo
 from functools import reduce
+import os
+
+DEBUG = os.environ.get('DEBUG')
 
 def addAdjacent():
     "Add elements touching displayed elements"
@@ -48,3 +51,225 @@ def addAdjacent():
         reduce(lambda a, b: a + len(b[1]), adjacentElements, 0)))
 
 
+def addAttached():
+    "Add all elements attached to displayed elements"
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+
+    activeElements = vp.getActiveElementLabels(printResults=False)
+    attachedElements = [] # elements attached to activeElements to add to display group
+
+    for instName, elementLabels in activeElements.items():
+        inst = odb.rootAssembly.instances[instName]
+        elemLabelsSet = set(activeElements.get(instName, [])) # active intance elements
+
+        elemIndex = set() # index of attached elements
+        nodeToElemIndex = {} # map nodeId to element index
+        progressTime = 0
+        for eIndex, element in enumerate(inst.elements):
+            if 0 == eIndex%1000 and time() > progressTime:
+                progressTime = time() + 2.5 # update every 2.5 seconds
+                milestone(message='Checking {}'.format(instName),
+                        object='element', done=eIndex, total=len(inst.elements))
+            if element.label in elemLabelsSet:
+                elemIndex.add(eIndex)
+            for nid in element.connectivity:
+                nodeToElemIndex.setdefault(nid, []).append(eIndex)
+
+        elemIndexNew = set(elemIndex)
+        while len(elemIndexNew):
+            nodes = set() # collect node labels for all elemIndexNew elements
+            for eIndex in elemIndexNew:
+                nodes.update(inst.elements[eIndex].connectivity)
+            for nid in nodes: # add elements using node labels
+                elemIndexNew.update(nodeToElemIndex[nid])
+            elemIndexNew.difference_update(elemIndex) # just the freshest elements
+            elemIndex.update(elemIndexNew)
+
+        if elemIndex:
+            addElements = set([inst.elements[eIndex].label
+                for eIndex in elemIndex]).difference(elementLabels)
+            attachedElements.append([instName, list(addElements)])
+
+    if attachedElements:
+        vp.odbDisplay.displayGroup.add(
+            leaf=dgo.LeafFromModelElemLabels(elementLabels=attachedElements))
+
+
+def addIncompleteSections():
+    """Add entire sections for active elements"""
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+    ra = odb.rootAssembly
+    elementSections = set()
+    activeElements = vp.getActiveElementLabels(printResults=False)
+    for instanceName, elementLabels in activeElements.items():
+        instance = ra.instances[instanceName]
+        for sa in instance.sectionAssignments:
+            saRegion = {element.label for element in sa.region.elements}
+            if saRegion.isdisjoint(elementLabels):
+                continue # no match
+            elementSections.add('.'.join([instance.name, sa.region.name, sa.sectionName]))
+    leaf = dgo.LeafFromOdbElementSections(elementSections=list(elementSections))
+    vp.odbDisplay.displayGroup.add(leaf=leaf)
+
+
+def addNearby(r=None):
+    "Add elements with nodes close to active nodes"
+    import numpy as np
+    try:
+        from scipy.spatial import KDTree
+    except ImportError:
+        getWarningReply('"Add nearby" requires Abaqus CAE 2020 or greater', (CANCEL,))
+        return
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+
+    if r is None:
+        r = getInput('Enter search radius:', '5')
+        if r is None:  # cancelled
+            return
+        r = float(r)
+
+    activeNodes = vp.getActiveNodeLabels(printResults=False)
+    activePoints = []
+    t0 = time()
+    if DEBUG: print(time() - t0, 'Collect active points')
+    for instName, nodeLabels in activeNodes.items():
+        inst = odb.rootAssembly.instances[instName]
+        activePoints.extend([inst.getNodeFromLabel(n).coordinates for n in nodeLabels])
+    if DEBUG: print(time() - t0, 'Make active KDTree', len(activePoints))
+    activeTree = KDTree(activePoints)
+
+    activeElements = vp.getActiveElementLabels(printResults=False)
+    nearbyElements = []
+    for inst in odb.rootAssembly.instances.values():
+        if DEBUG: print(time() - t0, 'Collect', inst.name, 'points')
+        points = [node.coordinates for node in inst.nodes]
+        if DEBUG: print(time() - t0, 'Make inst KDTree', len(points))
+        instTree = KDTree(points)
+        if DEBUG: print(time() - t0, 'Compare trees')
+        nIndex = set()
+        nIndex.update(*activeTree.query_ball_tree(instTree, r))
+        nodeLabels = {inst.nodes[i].label for i in nIndex}
+        if DEBUG: print(time() - t0, 'Find attached elements', len(nodeLabels))
+
+        elemSet = set(activeElements.get(instName, [])) # active intance elements
+        elements = []
+        progressTime = 0
+        for eIndex, element in enumerate(inst.elements):
+            if 0 == eIndex%1000 and time() > progressTime:
+                progressTime = time() + 2.5 # update every 2.5 seconds
+                milestone(message='Checking {}'.format(inst.name),
+                        object='element', done=eIndex, total=len(inst.elements))
+            if element.label in elemSet:
+                continue # already displayed
+            if not nodeLabels.isdisjoint(element.connectivity):
+                elements.append(element.label)
+        if elements:
+            nearbyElements.append([instName, elements])
+    if nearbyElements:
+        leaf = dgo.LeafFromModelElemLabels(elementLabels=nearbyElements)
+        vp.odbDisplay.displayGroup.add(leaf=leaf)
+    if DEBUG: print(time() - t0, 'Done')
+    print('Added {} nearby elements.'.format(
+        reduce(lambda a, b: a + len(b[1]), nearbyElements, 0)))
+
+
+def createFromSections():
+    """Create named display groups based on section definitions"""
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+    ra = odb.rootAssembly
+    sections = {}
+    for instance in ra.instances.values():
+        for sa in instance.sectionAssignments:
+            elementLabels = [element.label for element in sa.region.elements]
+            sections.setdefault(sa.sectionName, []).append(
+                    [instance.name, elementLabels] )
+    for sectionName, elementLabels in sections.items():
+        if session.displayGroups.has_key(sectionName):
+            del session.displayGroups[sectionName] # remove existing displayGroup
+        session.DisplayGroup(
+            leaf=dgo.LeafFromModelElemLabels(elementLabels=elementLabels),
+            name=sectionName)
+    print('Created {} display groups.'.format(len(sections)))
+
+
+def listActiveSections():
+    """List active sections, materials, and elements"""
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+    ra = odb.rootAssembly
+
+    activeElements = vp.getActiveElementLabels(printResults=False)
+    sections = []
+    for instanceName, elementLabels in activeElements.items():
+        instance = ra.instances[instanceName]
+        for sa in instance.sectionAssignments:
+            saRegion = {element.label for element in sa.region.elements}
+            common = saRegion.intersection(elementLabels)
+            if not common:
+                continue # no match
+            material = ''
+            if sa.sectionName in odb.sections:
+                section = odb.sections[sa.sectionName]
+                if hasattr(section, 'material') and section.material:
+                    material = section.material
+            sections.append([sa.sectionName, material, len(common)])
+    for section, material, nElements in sorted(sections):
+        print('{:40s} {:30s} {} elements'.format(section, material, nElements))
+
+
+def removeElement(element):
+    """Hide selected element"""
+    vp = session.viewports[session.currentViewportName]
+    leaf = dgo.LeafFromElementLabels(element.instanceName, [element.label])
+    vp.odbDisplay.displayGroup.remove(leaf=leaf)
+
+
+def removeElements(elements):
+    """Hide selected elements"""
+    vp = session.viewports[session.currentViewportName]
+    instElements = {}
+    for element in elements:  # group elements by instanceName
+        instElements.setdefault(element.instanceName, []).append(element.label)
+    for instanceName, elementLabels in instElements.items():
+        leaf = dgo.LeafFromElementLabels(instanceName, elementLabels)
+        vp.odbDisplay.displayGroup.remove(leaf=leaf)
+
+
+def removeSection(element):
+    """Hide all elements of the selected section"""
+    vp = session.viewports[session.currentViewportName]
+    odb = vp.displayedObject
+    if not hasattr(odb, 'rootAssembly'):
+        getWarningReply('Must have odb displayed in viewport', (CANCEL,))
+        return
+    ra = odb.rootAssembly
+    instance = ra.instances[element.instanceName]
+    for sa in instance.sectionAssignments: # find section for this element
+        if element in sa.region.elements:
+            break
+    else:
+        getWarningReply('No section assignment found for selected element', (CANCEL,))
+        return
+    elementSection = '.'.join([instance.name, sa.region.name, sa.sectionName])
+    print(elementSection)
+    leaf = dgo.LeafFromOdbElementSections(elementSections=(elementSection, ))
+    vp.odbDisplay.displayGroup.remove(leaf=leaf)
--- displayGroup_plugin.py
+++ displayGroup_plugin.py
@@ -3,20 +3,146 @@
 Carl Osterwisch, September 2017
 """
 
-__VERSION__ = 0.1
+__version__ = "0.2.0"
 
 from abaqusGui import *
-from abaqusConstants import *
+
+
+class ElementSelectProcedure(AFXProcedure):
+    """Class to allow user to select elements and run an assemblyMod command"""
+
+    def __init__(self, owner, prompt, method, number=MANY):
+        AFXProcedure.__init__(self, owner) # Construct the base class.
+        self._prompt = prompt
+        self._number = number
+
+        # Command
+        command = AFXGuiCommand(mode=self,
+                method=method,
+                objectName='displayGroup',
+                registerQuery=FALSE)
+
+        # Keywords
+        name = 'element'
+        if MANY == number:
+            name += 's'
+        self.elementsKw = AFXObjectKeyword(
+                command=command,
+                name=name,
+                isRequired=TRUE)
+
+        self.step1 = AFXPickStep(
+                owner=self,
+                keyword=self.elementsKw,
+                prompt='Select ' + self._prompt,
+                entitiesToPick=ELEMENTS,
+                numberToPick=self._number,
+                sequenceStyle=TUPLE)    # TUPLE or ARRAY
+
+    def getFirstStep(self):
+        return self.step1
+
+    def getLoopStep(self):
+        return self.step1 # repeat until cancelled
+
+###############################################################################
+# Register the plugins
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 
 toolset.registerKernelMenuButton(
         moduleName='displayGroup',
         functionName='addAdjacent()',
-        buttonText='Display Group|&Add Adjacent',
+        buttonText='Display Group|Add ad&jacent elements',
         author='Carl Osterwisch',
         description='Add elements that are attached to active nodes.',
-        version=str(__VERSION__),
-        applicableModules=['Mesh', 'Visualization'],
+        version=__version__,
+        applicableModules=['Visualization'],
     )
 
+
+toolset.registerKernelMenuButton(
+        moduleName='displayGroup',
+        functionName='addAttached()',
+        buttonText='Display Group|Add a&ttached elements',
+        author='Carl Osterwisch',
+        description='Add elements that are attached to active elements.',
+        version=__version__,
+        applicableModules=['Visualization'],
+    )
+
+
+toolset.registerKernelMenuButton(
+        moduleName='displayGroup',
+        functionName='addNearby()',
+        buttonText='Display Group|Add &nearby elements...',
+        author='Carl Osterwisch',
+        description='Add elements with nodes close to active nodes.',
+        version=__version__,
+        applicableModules=['Visualization'],
+    )
+
+
+toolset.registerKernelMenuButton(
+        moduleName='displayGroup',
+        functionName='addIncompleteSections()',
+        buttonText='Display Group|Add &same section elements',
+        author='Carl Osterwisch',
+        description='Add elements with same section assignment as active elements.',
+        version=__version__,
+        applicableModules=['Visualization'],
+    )
+
+
+toolset.registerKernelMenuButton(
+        moduleName='displayGroup',
+        functionName='createFromSections()',
+        buttonText='Display Group|Create &named from section assignments',
+        author='Carl Osterwisch',
+        description='Create a named display group for each section definition.',
+        version=__version__,
+        applicableModules=['Visualization'],
+    )
+
+
+toolset.registerKernelMenuButton(
+        moduleName='displayGroup',
+        functionName='listActiveSections()',
+        buttonText='Display Group|&List active sections',
+        author='Carl Osterwisch',
+        description='List sections for displayed elements.',
+        version=__version__,
+        applicableModules=['Visualization'],
+    )
+
+
+class RemoveElementsPicked(ElementSelectProcedure):
+    """CAE seems to register this class with the GuiMenuButton, not the instance of the class"""
+    pass
+
+toolset.registerGuiMenuButton(
+        buttonText='Display Group|Remove picked &elements',
+        object=RemoveElementsPicked(toolset, 'elements to remove', 'removeElements', MANY),
+        kernelInitString='import displayGroup',
+        author='Carl Osterwisch',
+        version=__version__,
+        helpUrl='https://github.com/costerwi',
+        applicableModules=['Visualization', ],
+        description='Hide elements selected in viewport and repeat.'
+    )
+
+
+class RemoveSectionPicked(ElementSelectProcedure):
+    """CAE seems to register this class with the GuiMenuButton, not the instance of the class"""
+    pass
+
+toolset.registerGuiMenuButton(
+        buttonText='Display Group|Remove picked &section assignment',
+        object=RemoveSectionPicked(toolset, 'section to remove', 'removeSection', ONE),
+        kernelInitString='import displayGroup',
+        author='Carl Osterwisch',
+        version=__version__,
+        helpUrl='https://github.com/costerwi',
+        applicableModules=['Visualization', ],
+        description='Hide all elements of the selected section and repeat.'
+    )
